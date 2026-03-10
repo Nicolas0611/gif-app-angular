ee465881c21d8ffcf42dba13a10ff4bd36a1c74c
Nicolás Sierra <nico.sierra13@gmail.com>
2026-03-09 22:14:15 -0500

    Refactor gif-list component to use Gif interface. Updated template and TypeScript class to iterate over GIF objects instead of image URLs, enhancing type safety and clarity in data handling.

--- src/app/gifs/components/gif-list/gif-list.ts
+++ src/app/gifs/components/gif-list/gif-list.ts
@@ -1,4 +1,5 @@
 import { Component, input } from '@angular/core';
+import { Gif } from '../../interfaces/gif.interface';
 import { GifListItem } from '../gif-list-item/gif-list-item';
 
 @Component({
@@ -7,5 +8,5 @@ import { GifListItem } from '../gif-list-item/gif-list-item';
   templateUrl: './gif-list.html',
 })
 export class GifList {
-  imageUrls = input.required<string[]>();
+  gifs = input.required<Gif[]>();
 }
