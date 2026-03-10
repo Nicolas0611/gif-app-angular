3092913e2626f6ce655addcc5141d0b885944b3a
Nicolás Sierra <nico.sierra13@gmail.com>
2026-03-09 22:14:06 -0500

    Update environment configuration files to include Giphy API settings. Added giphyApiKey and giphyApiUrl to both development and production environments for GIFs API integration.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  companyName: 'GifApp',
+
+  //Api Key Gif
+  giphyApiKey: 'WIKmOGFKyA0Bs2JwxDBo0bb6WaQmtxSq',
+  giphyApiUrl: 'https://api.giphy.com/v1',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
   companyName: 'GifApp',
+
+  //Api Key Gif
+  giphyApiKey: 'WIKmOGFKyA0Bs2JwxDBo0bb6WaQmtxSq',
+  giphyApiUrl: 'https://api.giphy.com/v1',
 };
