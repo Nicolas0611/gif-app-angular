ef45f53d9b68ba0b33abb6c388b08e3c38ac4550
Nicolás Sierra <nico.sierra13@gmail.com>
2026-03-11 19:22:32 -0500

    Implement search functionality in SearchPage component. Updated template to include a search input and button for GIF queries, and integrated GifsService to fetch results, enhancing user interaction and data handling.

--- src/app/gifs/pages/search-page/search-page.ts
+++ src/app/gifs/pages/search-page/search-page.ts
@@ -1,9 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { GifList } from '../../components/gif-list/gif-list';
+import { Gif } from '../../interfaces/gif.interface';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifList],
   templateUrl: './search-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPage {}
+export default class SearchPage {
+  gifs = signal<Gif[]>([]);
+  gifsService = inject(GifsService);
+
+  onSearchGifs(query: string) {
+    this.gifsService.searchGifs(query).subscribe((data) => {
+      this.gifs.set(data);
+    });
+  }
+}
