daed9847858ae23431e671a865ee4f7c39e4eca0
Nicolás Sierra <nico.sierra13@gmail.com>
2026-03-15 21:35:49 -0500

    Refactor TrendingPage to display grouped trending GIFs and implement scroll handling. Updated template to render GIFs in a grid layout and added a method for handling scroll events, enhancing user experience and interaction with the trending GIFs.

--- src/app/gifs/pages/trending-page/trending-page.ts
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -1,13 +1,18 @@
-import { Component, computed, inject } from '@angular/core';
-import { GifList } from '../../components/gif-list/gif-list';
+import { Component, inject } from '@angular/core';
 import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifList],
+  /*   imports: [GifList],
+   */
   templateUrl: './trending-page.html',
 })
 export default class TrendingPage {
   gifsService = inject(GifsService);
-  gifs = computed(() => this.gifsService.trendingGifs());
+  /*   gifs = computed(() => this.gifsService.trendingGifs());
+   */
+
+  onScroll(event: Event) {
+    console.log(event);
+  }
 }
